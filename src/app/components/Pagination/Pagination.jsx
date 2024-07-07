3590520a069e691d41c9df79dc1cde4a6e072367
Samrat Neupane <71063668+SamratNeupane787@users.noreply.github.com>
2024-07-07 15:33:46 +0545

    pagination

--- src/app/components/Pagination/Pagination.jsx
+++ src/app/components/Pagination/Pagination.jsx
@@ -1,11 +1,9 @@
 "use client";
 
 import React from "react";
-import styles from "./pagination.module.css";
+import styles from "./Pagination.module.css";
 
 const Pagination = () => {
- 
-
   return (
     <div className={styles.container}>
       <button className={styles.button}>Previous</button>
